80def028e552a4ff6d0d13da3313506f77a61d07
Albara <albara055055@hotmail.com>
2021-07-13 17:22:49 +0300

    showTime scraping

--- Movies.js
+++ Movies.js
@@ -1,24 +1,54 @@
 const puppeteer = require("puppeteer");
 
-const url = "https://ksa.voxcinemas.com/movies/whatson";
+const homePage = "https://ksa.voxcinemas.com/movies/whatson";
+const showTimes = "https://ksa.voxcinemas.com/showtimes?c=al-qasr-mall-riyadh&c=atyaf-mall-riyadh&c=kingdom-centre-riyadh&c=riyadh-front-riyadh&c=riyadh-park-riyadh&c=sahara-mall-riyadh&c=the-roof-riyadh&c=the-spot-sheikh-jaber-riyadh";
+
+// (async function main() {
+  
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   await page.goto(homePage);
+//   await page.waitForSelector(".movie-summary");
+
+//   const movies = await page.$$(".movie-summary");
+
+//   let m = [];
+
+//   await Promise.all(movies.map(async (movie) => {
+//     let p = await movie.$eval('.poster', (img) => img.getAttribute('data-src'));
+//     let lang = await movie.$eval(".language", (p) => p.innerText);
+//     m.push({
+//       Name: await movie.$eval("h3", (h3) => h3.innerText),
+//       classification: await movie.$eval("p", (p) => p.innerText),
+//       Language: lang.substr(10),
+//       poster: p == null || p.startsWith("/assets") ? await movie.$eval('.poster', (img) => img.getAttribute('src')) : p
+//     });
+//   }));
+//   console.log(m);
+//   await browser.close();
+// })();
 
 (async function main() {
+  
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto(url);
-  await page.waitForSelector(".movie-summary");
+  await page.goto(showTimes);
+  await page.waitForSelector(".movie-hero");
 
-  const movies = await page.$$(".movie-summary");
+  const voxMovieShowTime = await page.$$(".movie-compare");
+  let voxListShowTimes = [];
+  
+  await Promise.all(voxMovieShowTime.map(async (movie) => {
+    voxListShowTimes.push({
+      MovieName: await movie.$eval(".movie-hero>div>h2",(h2)=> h2.innerText),
+      MovieTime: await movie.$eval('.movie-hero>div>span:nth-child(4)',(a)=> a.innerText),
+      Place: await movie.$eval(".highlight", (h3)=> h3.innerText),
+      showRoom: await movie.$eval(".dates>ol>li>strong", (text)=> text.innerText),
 
-  let mov = { Name: "", classification: "", lang: "" };
-
-  for (const movie of movies) {
-    mov.Name = await movie.$eval("h3", (h3) => h3.innerText);
-    mov.classification = await movie.$eval("p", (p) => p.innerText);
-    mov.lang = await movie.$eval(".language", (p) => p.innerText);
-    console.log(JSON.stringify(mov));
-  }
-
+    });
+  }));
+  console.log(voxListShowTimes);
   await browser.close();
 })();
