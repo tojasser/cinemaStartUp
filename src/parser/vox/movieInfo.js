d0452212c9c2f5016c8f5d90cca38e1787908241
tojasser <tojasser@gmail.com>
2021-08-03 13:26:00 +0300

    complated parser for Riyadh movies - VOX

--- src/parser/vox/movieInfo.js
+++ src/parser/vox/movieInfo.js
@@ -14,42 +14,55 @@ async function moviesAndShowtimes(){
         //console.log(data.data)
         const $ = cheerio.load(data)
         const article = 'body > div.container > main > section.showtimes > article'
-
+        const movieArr = []
         $(article).each((parentIndx, parentElm) =>{ // movies loop 
-        console.log(`articale counter: ${parentIndx}`)
+        //console.log(`articale counter: ${parentIndx}`)
         //console.log($(parentElm).children().attr('href'))
         // console.log($(article).text())
 
 
         $(parentElm).children().each((childIndx, chaildElm) => {
-            console.log(`child indx ${childIndx}`)
-
+            //console.log(`child indx ${childIndx}`)
+            const movieObj = {}
             if (childIndx === 0 ){  // parsing movie info 
-                console.log($(chaildElm).children().attr('data-src'))
-                const movieInfo = chaildElm
+                const movieInfo = chaildElm 
+                
+                movieObj.poster = $(chaildElm).children().attr('data-src')
                 $(movieInfo).find('aside > div').children().each((divIndx, divElm) =>{
-                    console.log(`movie info indx: ${divIndx}`)
-                    console.log($(divElm).text())
+                    //console.log(`movie info indx: ${divIndx}`)
+                    movieObj.name = divIndx ===0 ? $(divElm).text() : movieObj.name   
+                    movieObj.classification = divIndx ===1 ? $(divElm).text() : movieObj.classification   
+                    movieObj.lang = divIndx ===2 ? $(divElm).text() : movieObj.lang   
+                    movieObj.time = divIndx ===3 ? $(divElm).text() : movieObj.time   
+                   
+                    // console.log($(divElm).text())
                 })
+                //console.log(movieObj)
+                
                 //console.log($('h2').text())
 
             }else { // here is the beginggg of class:dates (places and showtimes)
+                movieObj.showTimes = []
                 const shows = chaildElm
                 $(shows).find('div > h3').each((lvl3Indx,lvl3Elm) =>{
-        
-                    
-                    console.log(`lvl3 indx ${lvl3Indx}`)
-                    console.log($(lvl3Elm).text())
-                    //console.log($(lvl3Elm).next().text())
+                        
+                    //console.log(`lvl3 indx ${lvl3Indx}`)
+                    const place = $(lvl3Elm).text()
+                    //console.log($(lvl3Elm).next().text()+"test11")
+
+                    const showTime = {}
+                    showTime.place = place
+                    showTime.times = []
                     $(lvl3Elm).next().find('li > ol > li > a').each((lvl4Indx, lvl4Elm) => { // parse movie time and showtime link
-
-                        console.log($(lvl4Elm).text())
-                        console.log($(lvl4Elm).attr('href'))
+                        const time = $(lvl4Elm).text()
+                        const href = $(lvl4Elm).attr('href')
+                        showTime.times.push(time,href)
                     })
-
+                    movieObj.showTimes.push(showTime)
                 })
-
+           
             }
+            movieArr.push(movieObj)
 
             //console.log($(chaildElm).children().text())
            // body > div.container > main > section.showtimes > article:nth-child(3) > div
@@ -57,11 +70,26 @@ async function moviesAndShowtimes(){
         })
 
         })
-
-        
-
+        // console.log("--------------------------------------------")
+        // console.log(JSON.stringify(movieArr))
+        return movieArr
     } catch(err){
         console.log(err)
     }
 }
-moviesAndShowtimes()
+
+
+// moviesAndShowtimes()
+
+async function show() {
+    const moview = await moviesAndShowtimes()
+    console.log("--------------------------------------------")
+    console.log("--------------------------------------------")
+    console.log("--------------------------------------------")
+
+    console.log(JSON.stringify(moview));
+}
+
+show()
+
+
